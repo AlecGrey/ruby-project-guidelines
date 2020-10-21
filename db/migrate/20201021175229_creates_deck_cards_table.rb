3b3b3a4ee324cea0131b281ba48a117bf99fd741
JcFace <jcv@gmail.com>
2020-10-21 11:04:59 -0700

    Second migrate file

--- db/migrate/20201021175229_creates_deck_cards_table.rb
+++
@@ -1,5 +0,0 @@
-class CreatesDeckCardsTable < ActiveRecord::Migration[6.0]
-  def change
-    
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_174602) do
+ActiveRecord::Schema.define(version: 2020_10_21_175229) do
 
   create_table "cards", force: :cascade do |t|
     t.string "name"
