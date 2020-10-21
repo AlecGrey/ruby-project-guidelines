65f79e432d80550fcb90d5fc0ca794bfe2348897
JcFace <jcv@gmail.com>
2020-10-21 14:47:48 -0700

    commit

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+
+response_string = RestClient.get("https://api.magicthegathering.io/v1/cards")
+response_hash = JSON.parse(response_string)
+cards = response_hash["cards"]
+arr = []
+cards.each do |c|
+    arr << {name: c["name"], color: c["colorIdentity"], type: c["type"]}
+end
+binding.pry
+arr
